fd2ba225d967fd142902139f5ec69797ccb3999f
dawit <dawitgem@gmail.com>
2024-04-05 21:56:01 -0400

    modified

--- client/.next/server/server-reference-manifest.js
+++ client/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kpR8ZOTbaA3AgHqZxW/LKG6aWJwtP6ZnjzixPUC+gIo=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BLjOFDFJWDjOd7PfOfCm5N0dqUZTBWODb5OgTyAOhQc=\"\n}"
