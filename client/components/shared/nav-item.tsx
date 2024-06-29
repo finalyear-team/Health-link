1539e4cbaff0dfa51d877c39675c6e210b136a64
lykamopia <www.alex94lykam@gmail.com>
2024-06-29 21:50:49 +0300

    add padding to nav item

--- client/components/shared/nav-item.tsx
+++ client/components/shared/nav-item.tsx
@@ -22,7 +22,7 @@ const NavItem = ({
   <TooltipProvider>
     <Link
       href={goto}
-      className={`flex items-center space-x-2 pr-4 ${
+      className={`flex items-center space-x-2 px-4 py-2 ${
         isActive
           ? "border-l-4 rounded-tr-sm rounded-br-sm border-primary-600 text-primary-600 dark:text-primary-700 dark:border-primary-700 bg-primary-50 dark:bg-slate-800"
           : "text-dark-700 hover:bg-slate-50 dark:hover:bg-slate-800 dark:text-slate-50"
